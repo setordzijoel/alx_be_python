8e7ce08316b957171deb6152eed6d63720150a8a
setordzijoel <setordzijoel@gmail.com>
2025-07-07 16:28:20 +0000

    Move basic_operations.py to python_introduction.

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+add = number1 + number2
+subt = number1 - number2
+mult = number1 * number2
+
+print("Addition of",number1,"and",number2,"is",add)
+print("Subtraction of",number1,"and",number2,"is",subt)
+print("Multiplication of",number1,"and",number2,"is",mult)
