151a60735ea78c61b1fd3e5627122335c735b9ec
setordzijoel <setordzijoel@gmail.com>
2025-07-27 23:16:44 +0000

    Modify

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,15 +1,15 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+global FAHRENHEIT_TO_CELSIUS_FACTOR
+global CELSIUS_TO_FAHRENHEIT_FACTOR
 
 def convert_to_celsius(fahrenheit):
-    global FAHRENHEIT_TO_CELSIUS_FACTOR
-    FAHRENHEIT_TO_CELSIUS_FACTOR = FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32)
-    return FAHRENHEIT_TO_CELSIUS_FACTOR
+    FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+    #FAHRENHEIT_TO_CELSIUS_FACTOR = FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32)
+    return FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32)
 
 def convert_to_fahrenheit(celsius):
-    global CELSIUS_TO_FAHRENHEIT_FACTOR
-    CELSIUS_TO_FAHRENHEIT_FACTOR = CELSIUS_TO_FAHRENHEIT_FACTOR * (celsius + 32)
-    return CELSIUS_TO_FAHRENHEIT_FACTOR
+    CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+    #CELSIUS_TO_FAHRENHEIT_FACTOR = CELSIUS_TO_FAHRENHEIT_FACTOR * (celsius + 32)
+    return CELSIUS_TO_FAHRENHEIT_FACTOR * (celsius + 32)
 
 temp = float(input("Enter the temperature to convert: "))
 temp_type = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
