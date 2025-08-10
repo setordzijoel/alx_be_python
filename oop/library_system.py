a7998434ca195e93a45bf0bd4061a63182b153e8
setordzijoel <setordzijoel@gmail.com>
2025-08-10 15:09:46 +0000

    Modify library_system.py file.

--- oop/library_system.py
+++ oop/library_system.py
@@ -6,7 +6,7 @@ class Book:
     def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
-class Ebook(Book):
+class EBook(Book):
     def __init__(self, title:str, author:str, file_size:int):
         super().__init__(title, author)
         self.file_size = file_size
