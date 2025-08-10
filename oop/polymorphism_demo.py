caf915271a7e995eadeea47976d96671e707aa8f
setordzijoel <setordzijoel@gmail.com>
2025-08-10 19:41:28 +0000

    Add and modify polymorphism_demo.py file.

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,19 @@
+class Shape:    
+    def area(self):
+        raise NotImplementedError("Derived classes need to override this method.")
+    
+class Rectangle(Shape):
+    def __init__(self, length:float, width:float):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+    
+class Circle(Shape):
+    def __init__(self, radius:float):
+        #super().__init__()
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
